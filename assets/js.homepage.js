01d6e3e34a693aec8a306b73fde27015505317d3
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-02-14 19:46:31 -0800

    Update js.homepage.js

--- assets/js.homepage.js
+++ assets/js.homepage.js
@@ -3,9 +3,32 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
+ response.json().then(function(data) {
+  displayRepos(data, user);
+});
+  
+      var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+      var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+};
     });
+    
   });
+  userFormEl.addEventListener("submit", formSubmitHandler);
+  event.preventDefault()
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+};
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
 };
