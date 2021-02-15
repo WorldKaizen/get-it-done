e143cdbff3c6902bf658aebfe519d2c455cc42b4
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-02-14 20:40:22 -0800

    Update js.homepage.js

--- assets/js.homepage.js
+++ assets/js.homepage.js
@@ -1,10 +1,34 @@
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+// clear old content
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent = searchTerm;
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
  response.json().then(function(data) {
-  displayRepos(data, user);
+  displayRepos // loop over repos
+for (var i = 0; i < repos.length; i++) {
+  // format repo name
+  var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+  // create a container for each repo
+  var repoEl = document.createElement("div");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+  // create a span element to hold repository name
+  var titleEl = document.createElement("span");
+  titleEl.textContent = repoName;
+
+  // append to container
+  repoEl.appendChild(titleEl);
+
+  // append container to the dom
+  repoContainerEl.appendChild(repoEl);
+}
 });
   
       var userFormEl = document.querySelector("#user-form");
